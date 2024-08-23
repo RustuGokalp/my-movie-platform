94204bebd46e30a5fbc6188253d148dc395d81fc
Gökalp Beğen <gokalp.begen@demirorenteknoloji.com>
2024-08-23 22:59:31 +0300

    Create Footer component

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️ by&nbsp;
+      <Link href="https://github.com/RustuGokalp" target="_blank">
+        Rüştü Gökalp Beğen
+      </Link>
+    </footer>
+  );
 };
 
 export default Footer;
