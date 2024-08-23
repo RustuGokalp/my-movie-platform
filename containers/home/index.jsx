d3c43a06944b16a76f6deb131cec8681a0f7a3b5
Gökalp Beğen <gokalp.begen@demirorenteknoloji.com>
2024-08-24 01:43:52 +0300

    Create categories component

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Movies from "@/mocks/movies.json";
+import Genres from "@/mocks/genres.json";
 import FeaturedMovies from "@/components/featured-movie";
+import Categories from "@/components/categories";
 
 const HomeContainer = () => {
   return (
     <div>
       <FeaturedMovies movie={Movies.results[0]} />
+      <Categories categories={Genres.genres} />
     </div>
   );
 };
