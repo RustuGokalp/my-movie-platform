d44dea06c5c7d5af63aa772084a20302559289ce
Gökalp Beğen <gokalp.begen@demirorenteknoloji.com>
2024-08-24 16:03:30 +0300

    Create not found page for movie detail page

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import MovieContainer from "@/containers/movie";
 import Movies from "@/mocks/movies.json";
+import { notFound } from "next/navigation";
 
 const MoviePage = ({ params }) => {
   const movieDetail = Movies.results.find((movie) => movie.id == params.id);
+
+  if (!movieDetail) {
+    notFound();
+  }
 
   return <MovieContainer movie={movieDetail} />;
 };
