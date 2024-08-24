9947622e697e9fd86b3fbfe2af59488e69e0edc0
Gökalp Beğen <gokalp.begen@demirorenteknoloji.com>
2024-08-24 16:41:56 +0300

    Create category area on homepage

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -5,11 +5,21 @@ import FeaturedMovies from "@/components/featured-movie/index";
 import Categories from "@/components/categories/index";
 import MoviesSection from "@/components/movies-section/index";
 
-const HomeContainer = () => {
+const HomeContainer = ({ selectedCategory }) => {
   return (
     <div>
       <FeaturedMovies movie={Movies.results[0]} />
       <Categories categories={Genres.genres} />
+      {selectedCategory?.movies?.length > 0 && (
+        <MoviesSection
+          title={
+            Genres.genres.find((category) => category.id == selectedCategory.id)
+              ?.name
+          }
+          movies={selectedCategory.movies}
+        />
+      )}
+
       <MoviesSection title="Popular Films" movies={Movies.results} />
     </div>
   );
