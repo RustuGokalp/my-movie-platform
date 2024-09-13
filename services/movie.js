ff49e50b594851a6e9ccdbf7db71d951e34d57be
Gökalp Beğen <RustuGokalp@users.noreply.github.com>
2024-09-13 12:41:34 +0300

    Add axios library to project and refactor requests

--- services/movie.js
+++ services/movie.js
@@ -1,32 +1,47 @@
+import axios from "axios";
+
 const API_URL = "https://api.themoviedb.org/3/";
+
 const fetchMovieApi = async (pathname, query = "") => {
   try {
-    const res = await fetch(
-      `${API_URL}${pathname}?api_key=${process.env.API_KEY}&${query}`
-    );
-    return res.json();
+    const response = await axios.get(`${API_URL}${pathname}`, {
+      params: {
+        api_key: process.env.API_KEY,
+        ...query,
+      },
+    });
+    return response.data;
   } catch (error) {
-    throw new Error(error);
+    throw new Error(
+      error.response ? error.response.data.status_message : error.message
+    );
   }
 };
+
 const getSingleCategory = async (genreId) => {
-  return fetchMovieApi("discover/movie", `with_genres=${genreId}`);
+  return fetchMovieApi("discover/movie", { with_genres: genreId });
 };
+
 const getPopularMovies = async () => {
-  return fetchMovieApi("movie/popular", `page=1`);
+  return fetchMovieApi("movie/popular", { page: 1 });
 };
+
 const getTopRatedMovies = async () => {
-  return fetchMovieApi("movie/top_rated", `page=1`);
+  return fetchMovieApi("movie/top_rated", { page: 1 });
 };
+
 const getCategories = async () => {
-  return fetchMovieApi("genre/movie/list", `page=1`);
+  return fetchMovieApi("genre/movie/list");
 };
+
 const getActors = async () => {
   return fetchMovieApi("person/popular");
 };
+
 const getActorDetail = async (id) => {
   return fetchMovieApi(`person/${id}`);
 };
+
 const getMovie = async (movieId) => {
   return fetchMovieApi(`movie/${movieId}`);
 };
