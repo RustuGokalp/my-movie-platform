7b22c395ed2d7fc94aa0148d31980f642dd22d04
Gökalp Beğen <RustuGokalp@users.noreply.github.com>
2024-09-15 01:14:00 +0300

    Handle authorization process!!

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -1,19 +1,73 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import LoginPage, { Logo, Password, Footer } from "@react-login-page/page6";
+import LoginPage, {
+  Logo,
+  Password,
+  Footer,
+  Username,
+} from "@react-login-page/page6";
+import {
+  createRequestToken,
+  validateWithLogin,
+  createSession,
+  handleAuthorization,
+} from "@/services/movie";
 
-const Login = () => (
-  <LoginPage style={{ height: "90vh" }}>
-    <Logo>
-      <Image src="/image.png" alt="My Image" width={55} height={50} />
-    </Logo>
-    <Password />
-    <Footer>
-      Not a member? <Link href="https://www.themoviedb.org/">Sign up now</Link>
-    </Footer>
-  </LoginPage>
-);
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const requestToken = await createRequestToken();
+
+      handleAuthorization(requestToken);
+
+      const validatedToken = await validateWithLogin(
+        username,
+        password,
+        requestToken
+      );
+
+      if (validatedToken) {
+        await createSession(validatedToken);
+      }
+    } catch (error) {
+      console.error("Login failed: ", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <LoginPage style={{ height: "90vh" }}>
+        <Logo>
+          <Image src="/image.png" alt="Netfilm Logo" width={55} height={50} />
+        </Logo>
+
+        <Username
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <Password
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+
+        <Footer>
+          Not a member?{" "}
+          <Link href="https://www.themoviedb.org/signup" target="_blank">
+            Sign up now
+          </Link>
+        </Footer>
+      </LoginPage>
+    </form>
+  );
+};
 
 export default Login;
--- services/movie.js
+++ services/movie.js
@@ -18,6 +18,123 @@ const fetchMovieApi = async (pathname, query = "") => {
   }
 };
 
+// Create Request Token
+const createRequestToken = async () => {
+  try {
+    const response = await axios.get(`${API_URL}authentication/token/new`, {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_AUTH}`,
+      },
+    });
+    return response.data.request_token;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.status_message : error.message
+    );
+  }
+};
+
+// Redirect User To TMDB
+const handleAuthorization = (requestToken) => {
+  const redirectUrl = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=http://localhost:3000/approved`; // Geri döneceği URL
+  window.open(redirectUrl, "_blank");
+};
+
+// Validate Request Token (Login)
+const validateWithLogin = async (username, password, requestToken) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}authentication/token/validate_with_login`,
+      {
+        username,
+        password,
+        request_token: requestToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_AUTH}`,
+        },
+      }
+    );
+    return response.data.request_token;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.status_message : error.message
+    );
+  }
+};
+
+// Create Session ID ve set it to localStorage
+const createSession = async (requestToken) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}authentication/session/new`,
+      {
+        request_token: requestToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_AUTH}`,
+        },
+      }
+    );
+
+    const sessionId = response.data.session_id;
+    localStorage.setItem("session_id", sessionId);
+
+    const accountResponse = await axios.get(`${API_URL}account`, {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_AUTH}`,
+      },
+      params: {
+        session_id: sessionId,
+      },
+    });
+    const accountId = accountResponse.data.id;
+    localStorage.setItem("account_id", accountId);
+
+    return { sessionId, accountId };
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.status_message : error.message
+    );
+  }
+};
+
+const addToWatchlist = async (movieId) => {
+  const sessionId = localStorage.getItem("session_id");
+  const accountId = localStorage.getItem("account_id");
+
+  if (!sessionId || !accountId) {
+    throw new Error("Session ID or Account ID is missing");
+  }
+
+  try {
+    const response = await axios.post(
+      `${API_URL}account/${accountId}/watchlist`,
+      {
+        media_type: "movie",
+        media_id: movieId,
+        watchlist: true,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_AUTH}`,
+        },
+        params: {
+          session_id: sessionId,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.status_message : error.message
+    );
+  }
+};
+
 const getSingleCategory = async (genreId) => {
   return fetchMovieApi("discover/movie", { with_genres: genreId });
 };
@@ -55,4 +172,9 @@ export {
   getActors,
   getActorDetail,
   getMovie,
+  createRequestToken,
+  handleAuthorization,
+  validateWithLogin,
+  createSession,
+  addToWatchlist,
 };
