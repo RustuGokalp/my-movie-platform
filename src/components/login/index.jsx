f738bd3d8950c9ad09e45cd7cd5f7174826a64d2
Gökalp Beğen <RustuGokalp@users.noreply.github.com>
2024-09-17 23:43:46 +0300

    Add react modal package and use it in error & success situation in login page

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -9,13 +9,23 @@ import {
   validateWithLogin,
   createSession,
 } from "@/services/movie";
+import Modal from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
 import styles from "./styles.module.css";
 
 const Login = () => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
+
   const handleLogin = async (values, { setErrors }) => {
+    setLoading(true);
     try {
       const requestToken = await createRequestToken();
-
       const validatedToken = await validateWithLogin(
         values?.username,
         values?.password,
@@ -23,80 +33,116 @@ const Login = () => {
       );
       if (validatedToken) {
         await createSession(validatedToken);
+        setIsSuccess(true);
+        setModalMessage("Login successful!");
       }
     } catch (error) {
       const errorMessage = error.message || "Login failed. Please try again.";
       setErrors({
         server: errorMessage,
       });
+      setIsSuccess(false);
+      setModalMessage(errorMessage);
+    } finally {
+      setLoading(false);
+      openModal();
     }
   };
 
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      validationSchema={LoginSchema}
-      onSubmit={handleLogin}
-    >
-      {({ errors }) => (
-        <Form className={styles.loginPage}>
-          <div className={styles.logoContainer}>
-            <Image width={50} height={50} src="/image.png" alt="Netfilm Logo" />
-            <h3>NETFILMS</h3>
-          </div>
+    <>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={LoginSchema}
+        onSubmit={handleLogin}
+      >
+        {({ errors }) => (
+          <Form className={styles.loginPage}>
+            <div className={styles.logoContainer}>
+              <Image
+                width={50}
+                height={50}
+                src="/image.png"
+                alt="Netfilm Logo"
+              />
+              <h3>NETFILMS</h3>
+            </div>
 
-          <div className={styles.formGroup}>
-            <label htmlFor="username" className={styles.label}>
-              Username
-            </label>
-            <Field
-              id="username"
-              name="username"
-              placeholder="Username"
-              className={styles.inputField}
-            />
-            <ErrorMessage
-              name="username"
-              component="div"
-              className={styles.errorText}
-            />
-          </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="username" className={styles.label}>
+                Username
+              </label>
+              <Field
+                id="username"
+                name="username"
+                placeholder="Username"
+                className={styles.inputField}
+              />
+              <ErrorMessage
+                name="username"
+                component="div"
+                className={styles.errorText}
+              />
+            </div>
 
-          <div className={styles.formGroup}>
-            <label htmlFor="password" className={styles.label}>
-              Password
-            </label>
-            <Field
-              id="password"
-              name="password"
-              placeholder="Password"
-              type="password"
-              className={styles.inputField}
-            />
-            <ErrorMessage
-              name="password"
-              component="div"
-              className={styles.errorText}
-            />
-          </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="password" className={styles.label}>
+                Password
+              </label>
+              <Field
+                id="password"
+                name="password"
+                placeholder="Password"
+                type="password"
+                className={styles.inputField}
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={styles.errorText}
+              />
+            </div>
 
-          {errors.server && (
-            <div className={styles.errorText}>{errors.server}</div>
-          )}
+            {loading ? (
+              <div className={styles.loadingOverlay}>
+                <div className={styles.spinner}></div>
+              </div>
+            ) : (
+              <button type="submit" className={styles.submitButton}>
+                Login
+              </button>
+            )}
 
-          <button type="submit" className={styles.submitButton}>
-            Login
-          </button>
+            <Modal
+              open={open}
+              onClose={closeModal}
+              center
+              classNames={{
+                modal: styles.customModal,
+              }}
+            >
+              <div className={styles.modalContent}>
+                <Image
+                  className={styles.modelImg}
+                  src={isSuccess ? "/success.svg" : "/error.svg"}
+                  alt={isSuccess ? "Success" : "Error"}
+                  width={150}
+                  height={150}
+                />
+                <h2 className={styles.modalText}>{modalMessage}</h2>
+              </div>
+            </Modal>
 
-          <div className={styles.footer}>
-            Not a member?{" "}
-            <Link href="https://www.themoviedb.org/signup" target="_blank">
-              Sign up now
-            </Link>
-          </div>
-        </Form>
-      )}
-    </Formik>
+            <div className={styles.footer}>
+              Not a member?{" "}
+              <Link href="https://www.themoviedb.org/signup" target="_blank">
+                Sign up now
+              </Link>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
