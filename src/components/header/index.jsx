01ddb7d9199cde50b6cf8479735db2e4ad02098c
Gökalp Beğen <RustuGokalp@users.noreply.github.com>
2024-09-19 22:27:26 +0300

    Create avatar component and add condition to header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { FaPlayCircle } from "react-icons/fa";
 import styles from "./styles.module.css";
-
+import Avatar from "@/src/components/avatar";
 const Header = () => {
+  const [accountId, setAccountId] = useState(null);
+
+  useEffect(() => {
+    const storedAccountId = localStorage.getItem("account_id") ?? null;
+    setAccountId(storedAccountId);
+  }, [accountId]);
   return (
     <header className={`${styles.header} container fluid`}>
       <div className={styles.headerWrapper}>
@@ -14,7 +21,7 @@ const Header = () => {
           <Link href="/">Movies</Link>
           <Link href="/">Series</Link>
           <Link href="/actors">Actors</Link>
-          <Link href="/login">Login</Link>
+          {accountId ? <Avatar /> : <Link href="/login">Login</Link>}
         </nav>
       </div>
     </header>
