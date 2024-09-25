e79b78fad33b8a2af85e23a1003e6973e8ad8798
Gökalp Beğen <RustuGokalp@users.noreply.github.com>
2024-09-25 16:14:18 +0300

    Delete unnecessary comment lines

--- utils/validationShemas.js
+++ utils/validationShemas.js
@@ -3,10 +3,10 @@ import * as Yup from "yup";
 export const LoginSchema = Yup.object().shape({
   username: Yup.string()
     .min(4, "Username must be at least 4 characters long.")
-    .matches(/^(?!^\d+$).*/, "Username cannot contain only numbers.") // Yalnızca sayılardan oluşmasını engelle
+    .matches(/^(?!^\d+$).*/, "Username cannot contain only numbers.")
     .required("Username is required"),
   password: Yup.string()
     .min(4, "Please enter at least 4 characters")
-    .matches(/^(?!^\d+$).*/, "Password cannot contain only numbers.") // Yalnızca sayılardan oluşmasını engelle
+    .matches(/^(?!^\d+$).*/, "Password cannot contain only numbers.")
     .required("Password is required"),
 });
