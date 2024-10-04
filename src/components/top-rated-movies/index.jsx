a5b6bfec3b2998081a46d78cae8870516f1861fe
Gökalp Beğen <RustuGokalp@users.noreply.github.com>
2024-10-04 18:35:30 +0300

    Add sorting to the top rated movies page

--- src/components/top-rated-movies/index.jsx
+++ src/components/top-rated-movies/index.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "@mui/material";
 import MoviesSection from "@/src/components/movies-section";
 
 const TopRatedMoviesComp = ({ movies }) => {
-  return <MoviesSection title="Top Rated Movies" movies={movies} />;
+  const [sortedMovies, setSortedMovies] = useState([]);
+  const [sortOrder, setSortOrder] = useState("desc");
+
+  useEffect(() => {
+    if (movies.length > 0) {
+      sortMovies(sortOrder);
+    }
+  }, [movies]);
+
+  const sortMovies = (order) => {
+    setSortOrder(order);
+    const sorted = [...movies].sort((a, b) => {
+      if (order === "asc") {
+        return new Date(a?.popularity) - new Date(b?.popularity);
+      } else {
+        return new Date(b?.popularity) - new Date(a?.popularity);
+      }
+    });
+    setSortedMovies(sorted);
+  };
+  return (
+    <div>
+      <div
+        style={{
+          marginBottom: "16px",
+          display: "flex",
+          justifyContent: "center",
+          gap: "15px",
+          flexWrap: "wrap",
+        }}
+      >
+        <Button
+          variant={sortOrder === "desc" ? "contained" : "outlined"}
+          onClick={() => sortMovies("desc")}
+        >
+          Popularity Descending
+        </Button>
+        <Button
+          variant={sortOrder === "asc" ? "contained" : "outlined"}
+          onClick={() => sortMovies("asc")}
+        >
+          Popularity Ascending
+        </Button>
+      </div>
+      <MoviesSection title="Top Rated Movies" movies={sortedMovies ?? movies} />
+    </div>
+  );
 };
 
 export default TopRatedMoviesComp;
